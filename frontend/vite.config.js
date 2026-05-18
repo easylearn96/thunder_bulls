66c3ce241816a9d18ae7fccdea83b0e5bf29a68b
easylearn96 <mian.umarshafeeq78692@gmail.com>
2026-05-19 01:31:49 +0500

    chore: setup deployment to GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/thunder_bulls/',
   plugins: [react()],
   server: {
     proxy: {
